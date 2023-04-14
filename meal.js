12b9c19a97b89f4eba84a40f721ab534762807c3
Debajyoti Dutta <debajyotidutta.50@gmail.com>
2023-04-14 23:49:20 +0530

    final

---
+++ meal.js
@@ -0,0 +1,98 @@
+API_URL = 'https://www.themealdb.com/api/json/v1/1/lookup.php?i='
+
+let mainEle = document.getElementById('main')
+
+let itemPair = () => {
+    let attrDesc=document.createElement('div')
+    attrDesc.classList.add('attrdesc')
+    let attr=document.createElement('p')
+    attr.classList.add('attr')
+    let desc=document.createElement('p')
+    desc.classList.add('desc')
+    attrDesc.appendChild(attr)
+    attrDesc.appendChild(desc)
+    return attrDesc
+}
+
+let getIngridients=(data)=>{
+    console.log(data);
+    let olist=document.createElement('ol')
+    olist.classList.add('ingredients')
+    for(let i=1;i<=20;i++){
+        if (data['strIngredient'+i]=='') break
+        let liS=data['strIngredient'+i]+' => '+data['strMeasure'+i]
+        let liE=document.createElement('li')
+        liE.innerHTML=liS
+        olist.appendChild(liE)
+    }
+    return olist
+}
+
+let getVideo=(data)=>{
+    const iframe = document.createElement('iframe');
+    let yturl=data.strYoutube.split('=')
+    yturl=yturl[yturl.length-1]
+    // Set the attributes for the iframe element
+    iframe.setAttribute('width', '560');
+    iframe.setAttribute('height', '315');
+    iframe.setAttribute('src', 'https://www.youtube-nocookie.com/embed/'+yturl);
+    iframe.setAttribute('title', 'YouTube video player');
+    iframe.setAttribute('frameborder', '0');
+    iframe.setAttribute('allow', 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share');
+    iframe.setAttribute('allowfullscreen', '');
+    return iframe
+}
+let fillDetails = async() => {
+    let data= await fetchData()
+    console.log(data);
+    mainEle.innerHTML = ''
+
+    let mealImg = document.createElement('img')
+    mealImg.setAttribute('src', data.strMealThumb)
+
+    let name=itemPair()
+    name.children[0].innerHTML='Name : '
+    name.children[1].innerHTML=data.strMeal
+
+    let category=itemPair()
+    category.children[0].innerHTML='Category : '
+    category.children[1].innerHTML=data.strCategory
+
+    let area=itemPair()
+    area.children[0].innerHTML='Area : '
+    area.children[1].innerHTML=data.strArea
+
+    let instructions=itemPair()
+    instructions.children[0].innerHTML='Instructions : '
+    instructions.children[1].innerHTML=data.strInstructions
+
+    let tags=itemPair()
+    tags.children[0].innerHTML='Tags : '
+    tags.children[1].innerHTML=data.strTags
+
+    let ingredients=itemPair()
+    ingredients.children[0].innerHTML='Ingredients : '
+    ingredients.children[1].appendChild(getIngridients(data))
+
+    let video=itemPair()
+    video.children[0].innerHTML='Video Guide : '
+    video.children[1].appendChild(getVideo(data))
+
+    mainEle.appendChild(mealImg)
+    mainEle.appendChild(name)
+    mainEle.appendChild(category)
+    mainEle.appendChild(area)
+    mainEle.appendChild(instructions)
+    mainEle.appendChild(tags)
+    mainEle.appendChild(ingredients)
+    mainEle.appendChild(video)
+}
+
+let fetchData = async () => {
+    let mealID = window.location.search.split('=')[1]
+    console.log(mealID)
+    let data = await fetch(API_URL + mealID)
+    data = await data.json()
+    return data.meals[0]
+}
+fillDetails()
---
+++ script.js
@@ -0,0 +1,128 @@
+// Get DOM elements
+const searchInput = document.getElementById('search');
+const mealsContainer = document.getElementById('meals');
+const favButton = document.getElementById('favourites')
+let favIcons = document.querySelectorAll(".fa-heart")
+
+// Constants for API URL and delay time
+const API_URL = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
+const SEARCH_DELAY = 1000; // milliseconds
+
+// Variable to hold the timer object for delayed search requests
+let searchTimer;
+let favItemIds = {}
+let favOn = false
+
+let checkFavourite = (event) => {
+  console.log(event.target);
+  let idmeal = event.target.getAttribute('idmeal')
+  if (idmeal in favItemIds) {
+    delete favItemIds[idmeal]
+    event.target.style.color = 'blue'
+  }
+  else {
+    console.log(idmeal);
+    favItemIds[idmeal] = ''
+    event.target.style.color = 'red'
+  }
+}
+
+// Function to display meals on the page
+function displayMeals(meals) {
+  mealsContainer.innerHTML = ''; // clear previous search results
+  console.log(meals);
+  if (meals === null || meals.length === 0) {
+    mealsContainer.textContent = 'No results found.';
+    return;
+  }
+
+  meals.forEach(meal => {
+    const card = document.createElement('div');
+    card.classList.add('card');
+
+    const anchor=document.createElement('a')
+    anchor.setAttribute('href',window.location.origin+'/meal.html?id='+meal.idMeal)
+    anchor.setAttribute('target','_blank')
+
+    const image = document.createElement('img');
+    image.setAttribute('src', meal.strMealThumb);
+
+    const title = document.createElement('h3');
+    title.textContent = meal.strMeal;
+
+    const favoriteIcon = document.createElement('i');
+    favoriteIcon.classList.add('fa-solid', 'fa-heart')
+    favoriteIcon.style.color = (meal.idMeal in favItemIds) ? 'red' : 'blue'
+    favoriteIcon.setAttribute("idmeal", meal.idMeal)
+
+    anchor.appendChild(image)
+    card.appendChild(anchor)
+    // card.appendChild(image);
+    card.appendChild(title);
+    card.appendChild(favoriteIcon);
+    mealsContainer.appendChild(card);
+  });
+  favIcons = document.querySelectorAll(".fa-heart")
+  favIcons.forEach(icon => {
+    icon.addEventListener('click', checkFavourite)
+  })
+}
+
+// Function to fetch meals from the API based on search input
+async function fetchMeals(searchInput) {
+  try {
+    const response = await fetch(API_URL + searchInput);
+    const data = await response.json();
+    return data.meals;
+  } catch (error) {
+    console.error('Error fetching meals:', error);
+  }
+}
+
+// Function to delay search requests
+function delayedSearch(_event,delay) {
+  console.log(delay)
+  if (delay==null) delay=SEARCH_DELAY
+  console.log(delay)
+  clearInterval(searchTimer); // clear previous timer object
+  searchTimer = setTimeout(async () => {
+    const searchText = searchInput.value;
+    const meals = await fetchMeals(searchText);
+    displayMeals(meals);
+  }, delay);
+}
+
+// Event listener for search input
+searchInput.addEventListener('input', delayedSearch);
+
+
+
+// Immediately invoked async function to display initial meals on page load
+(async function () {
+  const meals = await fetchMeals('');
+  displayMeals(meals, 0);
+})();
+
+async function toggleFavourite() {
+  if (favOn) {
+    favOn = false;
+    searchInput.value = '';
+    delayedSearch(0,0);
+    console.log('favoff');
+  } else {
+    favOn = true;
+    const data = [];
+    const promises = Object.keys(favItemIds).map(async (id) => {
+      const response = await fetch(
+        `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`
+      );
+      const result = await response.json();
+      data.push(result.meals[0]);
+    });
+    await Promise.all(promises);
+    displayMeals(data, 0);
+    console.log('favon');
+  }
+}
+
+favButton.addEventListener('click', toggleFavourite)
